f9cd1eb2712e50745ea8b0ef6810cf77d43fac59
Neo <neoetichiro@gmail.com>
2025-03-25 10:19:34 +0100

    Création de la structure

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello Wold!'),
+          child: Text('Hello World!'),
         ),
       ),
     );

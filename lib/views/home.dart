522cd94df4cfaf9d21f85f4117dfcc4e3384016a
Neo <neoetichiro@gmail.com>
2025-03-25 10:53:37 +0100

    Changement de homescreen a mainscreen

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:elevate_quote_generator/widgets/quote_card.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
